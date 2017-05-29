71f7a83e2be3b8edb7029e775feaa34bb470ae73
Danrys <gallerydanrys@outlook.com>
2017-05-29 10:30:37 +0900

    Chang the saveDB to the pageinfo, add the information of  title, article_source, clicks and article.

--- iitg.py
+++ iitg.py
@@ -1,7 +1,8 @@
 import requests
 import re
 import pymysql
-
+import time
+from bs4 import BeautifulSoup
 def getHtmlText(url):
 	try:
 		r = requests.get(url, timeout = 30)
@@ -9,11 +10,11 @@ def getHtmlText(url):
 		r.encoding = r.apparent_encoding
 		return r.text
 	except:
-		print("error")
+		print("error2")
 		return "";
 		
 
-def parsePage(ilt, html):
+def parsePage(ilt, html, ulist):
 	try:
 		tlt = re.findall(r'<div class=\"s_tit\"><a href=\".*?\" target=\"_blank\">(.*?)</a>', html)
 		uat = re.findall(r'<div class=\"s_tit\"><a href=\"(.*?)\"', html)
@@ -23,9 +24,39 @@ def parsePage(ilt, html):
 			title = tlt[i]
 			url_atr = uat[i]
 			ilt.append([datemsg, title, url_atr])
+			ulist.append(url_atr)
+			
 	except:
 		return ""
 		
+		#<div style="text-align:center; margin:15px 0;">发布时间：2017-05-22 新闻来自：教务部 点击数：91</div>
+def parseArticlePage(ulist,pageInfoList):
+	try:
+		count = 0
+		tplt = "{}\t{}\t{}\t{}"
+		for i in ulist:
+			article_html = getHtmlText(i)
+			soup = BeautifulSoup(article_html, "html.parser")
+			
+			page = soup.find('div', {'id':'content_middle'})
+			title = page.find('h1').get_text()
+			info = page.find('div', {'style':'text-align:center; margin:15px 0;'}).get_text()
+			release_data = re.findall(r'发布时间：(\d{4}-\d{2}-\d{2})', info)
+			article_source = re.findall(r'新闻来自：(.*?) ', info)
+			clicks = re.findall(r'点击数：(\d+)', info)
+			articles = soup.findAll('p')
+			article = ""
+			for g in articles:
+				article = article + g.get_text()
+			replace_reg = re.compile(r'\xa0|\u3000')
+			article = replace_reg.sub(' ',article)
+			count += 1
+			print(tplt.format(count, release_data, article_source, clicks))
+			pageInfoList.append([release_data, title, article_source, clicks, article])
+			
+	except:
+		return ""
+	
 def printAtcList(ilt):
 	tplt = "{:4}\t{:8}\t{:16}"
 	print(tplt.format("序号", "日期", "名称"))
@@ -33,7 +64,23 @@ def printAtcList(ilt):
 	for g in ilt:
 		count += 1
 		print(tplt.format(count, g[0], g[1]))
+		
+def saveInfoIntoDB(pageInfoList):
+	conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='', db='douyucrawler', charset='utf8')
+	count = 0
+	SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS articleku(_id INT PRIMARY KEY AUTO_INCREMENT,redata VARCHAR(64) NOT NULL,title VARCHAR(256) NOT NULL,artsrc VARCHAR(256) NOT NULL,click VARCHAR(256) NOT NULL,article VARCHAR(1024) NOT NULL);"
+	SQL_INSERT = "INSERT INTO articleku(redata,title,artsrc,click,article) VALUES(%s,%s,%s,%s,%s)"
+	cursor = conn.cursor()
+	cursor.execute(SQL_CREATE_TABLE)
+	for k in pageInfoList:
+		count += 1
+		cursor.execute(SQL_INSERT,[k[0],k[1],k[2],k[3],k[4]])
+	cursor.close()
+	conn.commit()
+	conn.close()
+	
 def saveIntoDB(ilt):
+	print(ilt)
 	conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='', db='douyucrawler', charset='utf8')
 	tplt = "{:4}\t{:8}\t{:16}"
 	count = 0
@@ -43,26 +90,29 @@ def saveIntoDB(ilt):
 	cursor.execute(SQL_CREATE_TABLE)
 	for g in ilt:
 		count += 1
-		print(tplt.format(count, g[0], g[2]))
 		cursor.execute(SQL_INSERT,[g[0],g[1],g[2]])
 	cursor.close()
 	conn.commit()
 	conn.close()
 
 def main():
-	dept = 2
+	dept = 24
 	start_url = "http://i2.guet.edu.cn/jwb/list.asp?id=17&PageNo="
-	infoList = []
+	infoAndUrl = []
+	urlList = []
+	pageInfoList = []
 	for i in range(dept):
 		try:
 			url = start_url + str(i+1)
 			html = getHtmlText(url)
-			parsePage(infoList, html)
+			parsePage(infoAndUrl, html, urlList)
 		except:
-			print("error")
+			print("error1")
 			continue
-			
-	#printAtcList(infoList)
-	saveIntoDB(infoList)
+	
+	#printAtcList(infoAndUrl)
+	parseArticlePage(urlList, pageInfoList)
+	#saveIntoDB(infoList)
+	saveInfoIntoDB(pageInfoList)
 	
 main()
