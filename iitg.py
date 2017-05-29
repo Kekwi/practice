297ae68ad236d0bcd164799b64802a108f46930d
Danrys <gallerydanrys@outlook.com>
2017-05-29 22:49:50 +0900

    Change the Request URL, delect the useless function codes.

--- iitg.py
+++ iitg.py
@@ -11,108 +11,91 @@ def getHtmlText(url):
 		return r.text
 	except:
 		print("error2")
-		return "";
+		return""
 		
 
-def parsePage(ilt, html, ulist):
-	try:
-		tlt = re.findall(r'<div class=\"s_tit\"><a href=\".*?\" target=\"_blank\">(.*?)</a>', html)
-		uat = re.findall(r'<div class=\"s_tit\"><a href=\"(.*?)\"', html)
-		dlt = re.findall(r'<span class="list_title_r"> (\[\d{4}-\d{2}-\d{2}\])</span>', html)
-		for i in range(len(tlt)):
-			datemsg = dlt[i]
-			title = tlt[i]
-			url_atr = uat[i]
-			ilt.append([datemsg, title, url_atr])
-			ulist.append(url_atr)
-			
-	except:
-		return ""
+def getAnnouncementURL(html, ulist):
+
+		soup = BeautifulSoup(html, "html.parser")
+		a = soup.findAll('a', {'class':'aa'})
+	
+		for i in a:
+			try:
+				aURL = 'http://www.ccgp-shandong.gov.cn' + i.attrs['href']
+				title = i.attrs['title']
+				ulist.append(aURL)
+			except:
+				continue
 		
 		#<div style="text-align:center; margin:15px 0;">发布时间：2017-05-22 新闻来自：教务部 点击数：91</div>
-def parseArticlePage(ulist,pageInfoList):
-	try:
-		count = 0
-		tplt = "{}\t{}\t{}\t{}"
-		for i in ulist:
-			article_html = getHtmlText(i)
-			soup = BeautifulSoup(article_html, "html.parser")
-			
-			page = soup.find('div', {'id':'content_middle'})
-			title = page.find('h1').get_text()
-			info = page.find('div', {'style':'text-align:center; margin:15px 0;'}).get_text()
-			release_data = re.findall(r'发布时间：(\d{4}-\d{2}-\d{2})', info)
-			article_source = re.findall(r'新闻来自：(.*?) ', info)
-			clicks = re.findall(r'点击数：(\d+)', info)
-			articles = soup.findAll('p')
-			article = ""
-			for g in articles:
-				article = article + g.get_text()
-			replace_reg = re.compile(r'\xa0|\u3000')
-			article = replace_reg.sub(' ',article)
-			count += 1
-			print(tplt.format(count, release_data, article_source, clicks))
-			pageInfoList.append([release_data, title, article_source, clicks, article])
-			
-	except:
-		return ""
-	
-def printAtcList(ilt):
-	tplt = "{:4}\t{:8}\t{:16}"
-	print(tplt.format("序号", "日期", "名称"))
+def parseAnnouncementPage(ulist,pageInfoList):
 	count = 0
-	for g in ilt:
+	tplt = "{}\t{}\t{}"
+	
+	for i in ulist:
+		article_html = getHtmlText(i)
+		soup = BeautifulSoup(article_html, "html.parser")
+		message = soup.find('td',{'class','aa'}).get_text()
+		replace_reg = re.compile(r'\xa0|\u3000')
+		announceInfo = soup.findAll('td',{'class':'Font9'})
+		count1 = 0;
+		publisherText = ""
+		publishTimeText = ""
+		#print(announceInfo[0])
+		if len(announceInfo) == 0:
+			print(i)
+			publisherText = "error"
+			publishTimeText = i
+			message = "error"
+		else:
+			for j in announceInfo:
+				if count1 == 0:
+					publisherText = j.get_text()
+					count1 += 1
+				else:
+					publishTimeText = j.get_text()
+		#publisherText = announceInfo[0]
+		#publishTimeText = announceInfo[1]
+		#publisherText = "asdfad"
+		#publishTimeText = "5412.454" 
+		message = replace_reg.sub(' ',message)
 		count += 1
-		print(tplt.format(count, g[0], g[1]))
-		
+		print(tplt.format(count, publisherText, publishTimeText))
+		pageInfoList.append([publisherText, publishTimeText,message])
+
+	
 def saveInfoIntoDB(pageInfoList):
 	conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='', db='douyucrawler', charset='utf8')
 	count = 0
-	SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS articleku(_id INT PRIMARY KEY AUTO_INCREMENT,redata VARCHAR(64) NOT NULL,title VARCHAR(256) NOT NULL,artsrc VARCHAR(256) NOT NULL,click VARCHAR(256) NOT NULL,article VARCHAR(1024) NOT NULL);"
-	SQL_INSERT = "INSERT INTO articleku(redata,title,artsrc,click,article) VALUES(%s,%s,%s,%s,%s)"
+	SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS articleku(_id INT PRIMARY KEY AUTO_INCREMENT,publisherText VARCHAR(64) NOT NULL,publishTimeText VARCHAR(256) NOT NULL,message TEXT(10240) NOT NULL);"
+	SQL_INSERT = "INSERT INTO articleku(publisherText,publishTimeText,message) VALUES(%s,%s,%s)"
 	cursor = conn.cursor()
 	cursor.execute(SQL_CREATE_TABLE)
 	for k in pageInfoList:
 		count += 1
-		cursor.execute(SQL_INSERT,[k[0],k[1],k[2],k[3],k[4]])
+		cursor.execute(SQL_INSERT,[k[0],k[1],k[2]])
 	cursor.close()
 	conn.commit()
 	conn.close()
 	
-def saveIntoDB(ilt):
-	print(ilt)
-	conn = pymysql.connect(host='127.0.0.1', port=3306, user='root', password='', db='douyucrawler', charset='utf8')
-	tplt = "{:4}\t{:8}\t{:16}"
-	count = 0
-	SQL_CREATE_TABLE = "CREATE TABLE IF NOT EXISTS pysqltest(_id INT PRIMARY KEY AUTO_INCREMENT,datemsg VARCHAR(64) NOT NULL,title VARCHAR(256) NOT NULL,url_atr VARCHAR(256) NOT NULL);"
-	SQL_INSERT = "INSERT INTO pysqltest(datemsg,title,url_atr) VALUES(%s,%s,%s)"
-	cursor = conn.cursor()
-	cursor.execute(SQL_CREATE_TABLE)
-	for g in ilt:
-		count += 1
-		cursor.execute(SQL_INSERT,[g[0],g[1],g[2]])
-	cursor.close()
-	conn.commit()
-	conn.close()
-
 def main():
-	dept = 24
-	start_url = "http://i2.guet.edu.cn/jwb/list.asp?id=17&PageNo="
-	infoAndUrl = []
+
+	dept = 20
+	start_url = "http://www.ccgp-shandong.gov.cn/sdgp2014/site/channelall.jsp?colcode=0304&curpage="
 	urlList = []
 	pageInfoList = []
 	for i in range(dept):
 		try:
-			url = start_url + str(i+1)
-			html = getHtmlText(url)
-			parsePage(infoAndUrl, html, urlList)
+			page_url = start_url + str(i+1)
+			html = getHtmlText(page_url)
+			getAnnouncementURL(html, urlList)
 		except:
 			print("error1")
 			continue
 	
-	#printAtcList(infoAndUrl)
-	parseArticlePage(urlList, pageInfoList)
-	#saveIntoDB(infoList)
+	
+	parseAnnouncementPage(urlList, pageInfoList)
+	
 	saveInfoIntoDB(pageInfoList)
 	
 main()
